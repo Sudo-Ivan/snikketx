7925b062a908a573c4a08329eda806e409b98363
Ivan <e318cbc04468bd574db2b4523dddd710>
2026-09-12 05:27:17 -0500

    Dedupe nginx template blocks into shared snippets.

--- web-proxy/entrypoint.sh
+++ web-proxy/entrypoint.sh
@@ -9,6 +9,9 @@ PROTOS="${SNIKKET_TWEAK_WEB_PROXY_PROTOS:-http https}"
 if test -f "$CERT_PATH"; then
 	## Certs already exist - render and deploy configs
 	/usr/local/bin/render-template.sh "/etc/nginx/templates/snikket-common" "/etc/nginx/snippets/snikket-common.conf"
+	/usr/local/bin/render-template.sh "/etc/nginx/templates/snikket-tls" "/etc/nginx/snippets/snikket-tls.conf"
+	/usr/local/bin/render-template.sh "/etc/nginx/templates/snikket-proxy-headers" "/etc/nginx/snippets/snikket-proxy-headers.conf"
+	/usr/local/bin/render-template.sh "/etc/nginx/templates/snikket-upload" "/etc/nginx/snippets/snikket-upload.conf"
 	for proto in $PROTOS; do
 		/usr/local/bin/render-template.sh "/etc/nginx/templates/$proto" "/etc/nginx/sites-enabled/$proto";
 	done
